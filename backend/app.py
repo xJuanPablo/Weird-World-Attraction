ffd832ae1fc57bd00dda77ac715b7ef1fb29ade3
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-11-07 19:58:40 -0600

    Imported Python-env and began configuring database

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,22 @@
 # The Flask class is the primary class to make a Flask applicarion
 # jsonify converts Python objects into JSON format
 from flask import Flask, jsonify
+# Imports SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+# Import dotenv
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
 
 # creates an instance of the Flask class
 app = Flask(__name__)
+
+# This is to congigure the app
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('MYSQL_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 # This is defining a route that executes the get_articles() function 
 # methods = ['GET']: specifies that this route should respond to HTTP GET requests
